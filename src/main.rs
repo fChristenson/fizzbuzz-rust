0c412b5efd71d4f58aaced2de02d3a02051a41f3
Fredrik <fredrik.christenson@gmail.com>
2018-07-03 20:33:37 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(line) => {
+                if let Ok(number) = line.parse::<u32>() {
+                    fizz_buzz(number)
+                } else {
+                    println!("Please input a number")
+                }
+            }
+            _ => println!("Please input a number"),
+        }
+    }
+}
+
+fn fizz_buzz(number: u32) {
+    for index in 0..number {
+        if index % 3 == 0 && index % 5 == 0 {
+            println!("Fizzbuzz")
+        } else if index % 5 == 0 {
+            println!("Buzz")
+        } else if index % 3 == 0 {
+            println!("Fizz")
+        } else {
+            println!("{}", index)
+        }
+    }
+}
